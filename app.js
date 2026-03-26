1c0d3c3ef85af26d94dd3456833cc886ce9a9932
blossom-babs <saphashb@gmail.com>
2026-03-26 18:01:39 -0400

    decoupled app from index

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import usersRouter from "./routes/users.js";
+import authRouter from "./routes/auth.js";
+import itemsRouter from "./routes/items.js";
+import claimsRoutes from "./routes/claims.js";
+import uploadRouter from "./routes/upload.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.get("/", (_req, res) => {
+    res.json("Hello Team!");
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/items", itemsRouter);
+app.use("/api/claims", claimsRoutes);
+app.use("/api/upload", uploadRouter);
+
+export default app;
