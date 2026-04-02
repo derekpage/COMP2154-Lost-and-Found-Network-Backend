5aa825826739106ac9cc42e70d89b486bea3ae60
blossom-babs <saphashb@gmail.com>
2026-04-02 16:17:52 -0400

    Admin can assign claim

--- routes/claims.js
+++ routes/claims.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 
 import { authenticateToken } from "../middleware/auth.js";
+import { adminAuth } from "../middleware/adminAuth.js";
 import { createClaim, updateClaimStatus, getClaim, getClaimsInbox, getClaimById, withdrawClaim, assignClaim } from "../controllers/claimsController.js";
 
 const router = Router();
 
 router.post("/", authenticateToken, createClaim);
 router.put("/:id", authenticateToken, updateClaimStatus);
-router.put("/:id/assign", authenticateToken, assignClaim);
 router.get("/", authenticateToken, getClaim);
 // must come before /:id so Express doesn't treat "inbox" as an id param
 router.get("/inbox", authenticateToken, getClaimsInbox);
 router.get("/:id", authenticateToken, getClaimById);
+router.put("/:id/assign", adminAuth, assignClaim);
 router.delete("/:id/withdraw", authenticateToken, withdrawClaim);
 
 
