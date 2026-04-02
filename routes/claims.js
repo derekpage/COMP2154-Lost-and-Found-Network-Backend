b0d0abf9aee3923e30bcb2d075a97d3b33786db3
blossom-babs <saphashb@gmail.com>
2026-04-02 14:27:34 -0400

    Resolve open claims ticket

--- routes/claims.js
+++ routes/claims.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
 import { authenticateToken } from "../middleware/auth.js";
-import { createClaim, updateClaimStatus, getClaim, getClaimById, withdrawClaim } from "../controllers/claimsController.js";
+import { createClaim, updateClaimStatus, getClaim, getClaimsInbox, getClaimById, withdrawClaim } from "../controllers/claimsController.js";
 
 const router = Router();
 
 router.post("/", authenticateToken, createClaim);
 router.put("/:id", authenticateToken, updateClaimStatus);
 router.get("/", authenticateToken, getClaim);
+// must come before /:id so Express doesn't treat "inbox" as an id param
+router.get("/inbox", authenticateToken, getClaimsInbox);
 router.get("/:id", authenticateToken, getClaimById);
 router.delete("/:id/withdraw", authenticateToken, withdrawClaim);
 
